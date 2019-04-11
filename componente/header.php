f2803adfe65f358aff40d4b1c95ae0b7489415ec
Marco <mark.mamk@gmail.com>
2019-04-11 11:59:01 -0500

    Se armo el esqueleto

---
+++ componente/header.php
@@ -0,0 +1,14 @@
+<nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">
+      	<img src="assets/img/logo.png" width="85" height="85">
+      </a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">Home</a></li>
+        <li><a href="badges.html">The Conference</a></li>
+        <li><a href="collapsible.html">Speakers</a></li>
+        <li><a href="collapsible.html">Past Events</a></li>
+        <li><a href="collapsible.html">RSVP</a></li>
+      </ul>
+    </div>
+</nav>
--- index.php
+++ index.php
@@ -2,8 +2,161 @@
 <html>
 <head>
 	<title>Big Data Summit</title>
+  <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport">
+	<link rel="stylesheet" type="text/css" href="assets/css/materialize.css">
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="assets/js/jquery.min.js"></script>
+	<script type="text/javascript" src="assets/js/materialize.min.js"></script>
+  <style>
+  .col img{
+    width:100%;
+  }
+  @media only screen and (max-width: 600px) {
+    .parallax-container {
+        height: 150px;
+    }
+}
+  </style>
 </head>
 <body>
+    <?php include_once "componente/header.php"; ?>
+	  <div class="parallax-container">
+      <div class="parallax"><img src="https://66.media.tumblr.com/442a12264704676f2dc2438c332f33bf/tumblr_ppg95bbrxx1vg08vc_400.gif"></div>
+    </div>
+    <div class="section white">
+        <div class="row">
+	        <div class="col s11 m6 offset-m3">
+		        <h2 class="header">The Conference</h2>
+				<div class="row">
+				    <div class="col s6">Two Days of Powerful Talks</div>
+              <div class="col s3">03 April 2023</div>
+              <div class="col s3">09:00 am</div>
+				    </div>
+            <p class="grey-text text-darken-3 lighten-3">I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.​ This is a great space to write long text about your company and your services. You can use this space to go into a little more detail about your company.</p>
+            <p class="grey-text text-darken-3 lighten-3">Talk about your team and what services you provide. Tell your visitors the story of how you came up with the idea for your business and what makes you different from your competitors. Make your company stand out and show your visitors who you are.</p>
+            <h1>↓ Speakers</h1> 
+		    </div>
+        </div>
+    </div>
+    <div class="parallax-container">
+      <div class="parallax"><img src="https://66.media.tumblr.com/442a12264704676f2dc2438c332f33bf/tumblr_ppg95bbrxx1vg08vc_400.gif"></div>
+    </div>
+    <div class="section white">
+      <div class="row">
+	      <div class="col s11 m6 offset-m3">
+				  <div class="row">
+				    <div class="col s3"><div>930</div><div>Participants</div></div>
+            <div class="col s3"><div>250</div><div>Topics</div></div>
+            <div class="col s3"><div>40</div><div>Sessions</div></div>
+            <div class="col s3"><div>160</div><div>Speakers</div></div>
+          </div>
+				</div>
+		  </div>
+    </div>
+    <div class="section white">
+      <div class="row">
+	      <div class="col s11 m6 offset-m3">
+          <h1 class="header">Speakers</h1>
+          <h5 class="header">Proud to bring inspirational speakers from across the globe</h5>
+				  <div class="row">
+				    <div class="col s4"><img src="https://66.media.tumblr.com/442a12264704676f2dc2438c332f33bf/tumblr_ppg95bbrxx1vg08vc_400.gif"></div>
+            <div class="col s4"><img src="https://66.media.tumblr.com/442a12264704676f2dc2438c332f33bf/tumblr_ppg95bbrxx1vg08vc_400.gif"></div>
+            <div class="col s4"><img src="https://66.media.tumblr.com/442a12264704676f2dc2438c332f33bf/tumblr_ppg95bbrxx1vg08vc_400.gif"></div>
+          </div>
+				</div>
+		  </div>
+    </div>
+    <div class="section white">
+      <div class="row">
+	      <div class="col s11 m6 offset-m3">
+          <h1 class="header">Sponsors</h1>
+				  <div class="row">
+				    <div class="col s4"><img src="https://66.media.tumblr.com/442a12264704676f2dc2438c332f33bf/tumblr_ppg95bbrxx1vg08vc_400.gif"></div>
+            <div class="col s4"><img src="https://66.media.tumblr.com/442a12264704676f2dc2438c332f33bf/tumblr_ppg95bbrxx1vg08vc_400.gif"></div>
+            <div class="col s4"><img src="https://66.media.tumblr.com/442a12264704676f2dc2438c332f33bf/tumblr_ppg95bbrxx1vg08vc_400.gif"></div>
+          </div>
+				</div>
+		  </div>
+    </div>
+    <div class="parallax-container">
+      <div class="parallax"><img src="https://66.media.tumblr.com/442a12264704676f2dc2438c332f33bf/tumblr_ppg95bbrxx1vg08vc_400.gif"></div>
+    </div>
+    <div class="section white">
+      <div class="row">
+	      <div class="col s11 m6 offset-m3">
+          <h1 class="header">Past Events</h1>
+				  <div class="row">
+				    <div class="col s6"><img src="https://66.media.tumblr.com/442a12264704676f2dc2438c332f33bf/tumblr_ppg95bbrxx1vg08vc_400.gif"></div>
+            <div class="col s6"><img src="https://66.media.tumblr.com/442a12264704676f2dc2438c332f33bf/tumblr_ppg95bbrxx1vg08vc_400.gif"></div>          </div>
+				</div>
+		  </div>
+    </div>
+    <div class="section white">
+      <div class="row">
+	      <div class="col s11 m6 offset-m3">
+          <h1 class="header">RSVP</h1>
+				  <div class="row">
+				    <div class="col s5">
+              <p><h7>RSVP for our next event. See you soon!</h7></p>
+              <a class="btn-large disabled">Get Tickets</a>
+            </div>
+            <div class="col s7">
+              <h7>Subscribe to get news on all upcoming events</h7>  
+              <div class="row">
+                <form class="col s12">
+                  <div class="row">
+                    <div class="input-field col s12">
+                      <input id="email" type="email" class="validate">
+                      <label for="email">Email</label>
+                      <span class="helper-text" data-error="wrong" data-success="right"></span>
+                      <a class="btn-large disabled">Suscribe Now</a>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>          
+          </div>
+				</div>
+		  </div>
+    </div>
+    <div class="parallax-container">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15607.199444623631!2d-77.0370224!3d-12.0572872!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x21e996eeb5d06b7d!2sSheraton!5e0!3m2!1ses!2spe!4v1554997775499!5m2!1ses!2spe" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+    </div>
+    <div class="section white">
+      <div class="row">
+	      <div class="col s11 m6 offset-m3">
+				  <div class="row">
+				    <div class="col s4"><img src="assets/img/logo.png" style="Width:80px;"></div>
+            <div class="col s4">
+              <div>500 Terry Francois Street</div>
+              <div>San Francisco, CA 94158</div>
+              <div>info@mysite.com</div>
+            </div>
+            <div class="col s4">
+              <div>Tel: 123-456-7890</div>
+              <div>Fax: 123-456-7890</div>
+              <div>
+                <span>Facebook</span>
+                <span>Twitter</span>
+                <span>Instagram</span>
+              </div>
+            </div>
+
+          </div>
+          <div class="row">
+            <div class="col s11">© 2023 by DMC. Proudly created with DMC</div>
+          </div>
+          
+				</div>
+        
+		  </div>
+      
+    </div>
 
 </body>
+<script type="text/javascript">
+ $(document).ready(function(){
+    $('.parallax').parallax();
+  });
+</script>
 </html>
